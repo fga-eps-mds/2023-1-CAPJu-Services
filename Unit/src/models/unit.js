7d84c6d27bb5d021f93e4e372206c526b709f9a2
Sérgio Cipriano <sergiosacj@protonmail.com>
2023-05-17 16:41:57 -0300

    (#fga-eps-mds/2023-1-CAPJu-Doc#6) - Cria serviço para Unidades

---
+++ Unit/src/models/unit.js
@@ -0,0 +1,25 @@
+import { Model, DataTypes } from "sequelize";
+
+class Unit extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        idUnit: {
+          type: DataTypes.INTEGER,
+          primaryKey: true,
+          autoIncrement: true,
+        },
+        name: {
+          type: DataTypes.STRING,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        tableName: "unit",
+      }
+    );
+  }
+}
+
+export default Unit;
---
+++ Unit/src/routes/index.js
@@ -0,0 +1,15 @@
+import { Router } from "express";
+import UnitController from "../controllers/unit.js";
+
+const routes = Router();
+
+routes.get("/units", UnitController.index);
+routes.get("/unitAdmins/:id", UnitController.getAdminsByUnitId);
+
+routes.post("/newUnit", UnitController.store);
+
+routes.put("/updateUnit", UnitController.update);
+
+routes.delete("/deleteUnit", UnitController.delete);
+
+export default routes;
