556492fc54212a234e8cd4b1605ce2aaeb97450a
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-05-21 17:12:29 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#45) - Muda model de função para classe
    Co-authored-by: Sérgio Cipriano <sergiosacj@protonmail.com>

--- User/src/models/user.js
+++ User/src/models/user.js
@@ -1,41 +1,46 @@
-const UserModel = (sequelize, DataTypes) => {
-  const User = sequelize.define('Users', {
-    cpf: {
-      type: DataTypes.STRING(11),
-      primaryKey: true,
-    },
-    fullName: {
-      type: DataTypes.STRING(300),
-      allowNull: false,
-    },
-    email: {
-      type: DataTypes.STRING(300),
-      allowNull: false,
-    },
-    password: {
-      type: DataTypes.STRING(256),
-      allowNull: false,
-    },
-    accepted: {
-      type: DataTypes.BOOLEAN,
-      allowNull: false,
-    },
-    idUnit: {
-      type: DataTypes.INTEGER,
-      foreignKey: true,
-      allowNull: false,
-    },
-    idRole: {
-      type: DataTypes.INTEGER,
-      foreignKey: true,
-      allowNull: false,
-    },
-  }, {
-    sequelize,
-    tableName: "users",
-  })
-
-  return User
+import { Model, DataTypes } from "sequelize";
+import sequelizeConfig from "../config/sequelize.js"
+import { Sequelize } from "sequelize";
+class UserModel extends Model {
+  static init(sequelize) {
+    super.init(
+      {
+        cpf: {
+          type: DataTypes.STRING(11),
+          primaryKey: true,
+        },
+        fullName: {
+          type: DataTypes.STRING(300),
+          allowNull: false,
+        },
+        email: {
+          type: DataTypes.STRING(300),
+          allowNull: false,
+        },
+        password: {
+          type: DataTypes.STRING(256),
+          allowNull: false,
+        },
+        accepted: {
+          type: DataTypes.BOOLEAN,
+          allowNull: false,
+        },
+        idUnit: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+        idRole: {
+          type: DataTypes.INTEGER,
+          allowNull: false,
+        },
+      },
+      {
+        sequelize,
+        tableName: "users",
+      }
+    );
+  }
 }
-
+UserModel.init(sequelizeConfig, Sequelize.DataTypes);
 export default UserModel;
+
