e77b4849147c2a80d106f4eb401d86ef7635384d
Sérgio Cipriano <sergiosacj@protonmail.com>
2023-05-20 22:18:02 -0300

    (#fga-eps-mds/2023-1-CAPJu-Doc#101) - Cria serviço para Processos, Fluxos e Etapas

---
+++ Process/src/routes/index.js
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import FlowController from "../controllers/flow.js";
+import ProcessController from "../controllers/process.js";
+import StageController from "../controllers/stage.js";
+
+const routes = Router();
+
+export default routes;
