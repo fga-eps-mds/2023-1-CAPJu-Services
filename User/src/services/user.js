c23d266aadb79b5fa44a6bdcf06470fb1317b1b6
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-05-22 12:39:36 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#45) - Adiciona e cconfigura eslint
    Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>

--- User/src/services/user.js
+++ User/src/services/user.js
@@ -3,11 +3,11 @@ class UserService {
     this.user = UserModel;
   }
   async getAllUsers() {
-    return await this.user.findAll();
+    return this.user.findAll();
   }
 
   async getUserByCpf(cpf) {
-    return await this.user.findAll({ where: { cpf } });
+    return this.user.findAll({ where: { cpf } });
   }
 }
 
