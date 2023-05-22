14e922dad34bcca87404071b562d534926dc0948
faco400 <vd.souza1999@gmail.com>
2023-05-22 17:56:22 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#45) - Adiciona endpoint de login de usuario e remove retorno de senha
    Co-authored-by: victordsantoss <victor.samuelsantoss@gmail.com>

--- User/src/services/user.js
+++ User/src/services/user.js
@@ -3,23 +3,44 @@ class UserService {
     this.user = UserModel;
   }
   async getAllUsers() {
-    return this.user.findAll();
+    return this.user.findAll({
+      attributes: {
+        exclude: ['password'],
+      },
+    });
   }
 
   async getAcceptedUsers() {
     return this.user.findAll({
       where: { accepted: true },
+      attributes: {
+        exclude: ['password'],
+      },
     });
   }
 
   async getNoAcceptedUsers() {
     return this.user.findAll({
       where: { accepted: false },
+      attributes: {
+        exclude: ['password'],
+      },
     });
   }
 
   async getUserByCpf(cpf) {
-    return this.user.findAll({ where: { cpf } });
+    return this.user.findOne({
+      where: { cpf },
+      attributes: {
+        exclude: ['password'],
+      },
+    });
+  }
+
+  async getUserByCpfWithPassword(cpf) {
+    return this.user.findOne({
+      where: { cpf },
+    });
   }
 }
 
