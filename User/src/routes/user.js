ec59469102c6e35cb204c83a2d3aa929522d85f2
faco400 <vd.souza1999@gmail.com>
2023-05-22 23:19:52 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#45) - Adiciona endpoint para deletar solicitacoes
    Co-authored-by: victordsantoss <victor.samuelsantoss@gmail.com>

--- User/src/routes/user.js
+++ User/src/routes/user.js
@@ -9,7 +9,9 @@ UserRoutes.post('/login', userController.loginUser);
 UserRoutes.post('/newUser', userController.store);
 UserRoutes.post('/updateUser/:cpf', userController.updateUserEmail);
 UserRoutes.post('/updateUserRole', userController.updateUserRole);
+UserRoutes.post('/acceptRequest/:cpf', userController.acceptRequest);
 UserRoutes.post('/updateUserPassword/:cpf', userController.updateUserPassword);
 UserRoutes.delete('/deleteUser/:cpf', userController.deleteByCpf);
+UserRoutes.delete('/deleteRequest/:cpf', userController.deleteRequest);
 
 export default UserRoutes;
