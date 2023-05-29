6988e35c71977c253b366932b9add8ee50823620
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-05-29 13:20:23 -0300

    (#fga-eps-mds/2023-1-CAPJu-Doc#170) - Padroniza código e integra com serviço de usuário

---
+++ Unit/src/services/unit.js
@@ -0,0 +1,41 @@
+class UnitService {
+  constructor(UnitModel) {
+    this.unit = UnitModel;
+  }
+  async getAllUnits() {
+    return this.unit.findAll();
+  }
+
+  async createUnit(name) {
+    return this.unit.create({ name });
+  }
+
+  async getUnitById(idUnit) {
+    return this.unit.findOne({
+      where: { idUnit },
+    });
+  }
+
+  async updateUnit(idUnit, name) {
+    const unit = await this.getUnitById(idUnit);
+    if (unit) {
+      const [updatedRows] = await this.unit.update(
+        { name: name },
+        { where: { idUnit: idUnit } },
+      );
+      if (updatedRows) return true;
+    }
+    return false;
+  }
+
+  async deleteUnit(idUnit, name) {
+    const unit = await this.getUnitById(idUnit);
+    if (unit) {
+      const [updatedRows] = await this.unit.destroy({ where: { idUnit } });
+      if (updatedRows) return true;
+    }
+    return false;
+  }
+}
+
+export default UnitService;
