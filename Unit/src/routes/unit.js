5b1ad83aff1c7829b600110319f01cf4c635a1ec
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-05-29 14:35:55 -0300

    (#fga-eps-mds/2023-1-CAPJu-Doc#170) - Adiciona endpoint de remover usuário administrador de unidade

--- Unit/src/routes/unit.js
+++ Unit/src/routes/unit.js
@@ -5,10 +5,11 @@ const UnitRoutes = express.Router();
 const unitController = new UnitController();
 
 UnitRoutes.get('/units', unitController.getAllUnits);
+UnitRoutes.get('/unitAdmins/:idUnit', unitController.getAdminsByUnitId);
 UnitRoutes.post('/newUnit', unitController.store);
 UnitRoutes.put('/updateUnit', unitController.update);
-UnitRoutes.delete('/deleteUnit', unitController.delete);
-UnitRoutes.get('/unitAdmins/:idUnit', unitController.getAdminsByUnitId);
 UnitRoutes.put('/setUnitAdmin', unitController.setUnitAdmin);
+UnitRoutes.put('/removeUnitAdmin', unitController.removeUnitAdmin);
+UnitRoutes.delete('/deleteUnit', unitController.delete);
 
 export default UnitRoutes;
