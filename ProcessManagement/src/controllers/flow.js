8c549f0d202ba9dbe3c42e4dddede29d0327681a
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-06-03 12:12:48 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#178) - Adiciona servico e controller de fluxos
    Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>

--- ProcessManagement/src/controllers/flow.js
+++ ProcessManagement/src/controllers/flow.js
@@ -3,22 +3,22 @@ import models from '../models/_index.js';
 import FlowService from '../services/flow.js';
 
 export class FlowController {
-    constructor() {
-        this.flowService = new FlowService(models.Flow);
-    }
+  constructor() {
+    this.flowService = new FlowService(models.Flow);
+  }
 
-    getAllFlows = async (req, res) => {
-        try {
-            const priorities = await this.flowService.getAllFlows();
-            if (!priorities) {
-                return res
-                    .status(401)
-                    .json({ message: 'Não existem fluxos cadatradas' });
-            } else {
-                return res.status(200).json(priorities);
-            }
-        } catch (error) {
-            return res.status(500).json({ message: 'Erro ao buscar fluxo' });
-        }
-    };
+  getAllFlows = async (req, res) => {
+    try {
+      const priorities = await this.flowService.getAllFlows();
+      if (!priorities) {
+        return res
+          .status(401)
+          .json({ message: 'Não existem fluxos cadatradas' });
+      } else {
+        return res.status(200).json(priorities);
+      }
+    } catch (error) {
+      return res.status(500).json({ message: 'Erro ao buscar fluxo' });
+    }
+  };
 }
