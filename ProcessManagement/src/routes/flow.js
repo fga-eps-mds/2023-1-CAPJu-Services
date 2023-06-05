c767e349598f02747c559248d70a744074977ba6
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-06-05 17:34:22 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#178) - Adiciona endpoint de criar fluxo

--- ProcessManagement/src/routes/flow.js
+++ ProcessManagement/src/routes/flow.js
@@ -3,5 +3,6 @@ import controllers from '../controllers/_index.js';
 
 const FlowRoutes = express.Router();
 FlowRoutes.get('/', controllers.flowController.getAllFlows);
+FlowRoutes.post('/newFlow', controllers.flowController.store);
 
 export default FlowRoutes;
