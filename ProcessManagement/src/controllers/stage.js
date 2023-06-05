d9ad9ff61cd434f820674e275d93ba67ecd88a6b
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-06-05 15:46:03 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#178) - Adiciona CRUD de Stages

--- ProcessManagement/src/controllers/stage.js
+++ ProcessManagement/src/controllers/stage.js
@@ -20,4 +20,44 @@ export class StageController {
       return res.status(500).json({ message: 'Erro ao buscar etapas' });
     }
   };
+
+  getStageById = async (req, res) => {
+    try {
+      const { idStage } = req.params;
+      const stage = await this.stageService.getStageById(idStage);
+      if (!stage) {
+        return res.status(401).json({ error: 'Essa etapa não existe' });
+      } else {
+        return res.status(200).json(stage);
+      }
+    } catch (error) {
+      return res.status(500).json({ message: 'Erro ao buscar etapas' });
+    }
+  };
+
+  store = async (req, res) => {
+    const { name, idUnit, duration } = req.body;
+    try {
+      const data = {
+        name: name.toLowerCase(),
+        idUnit,
+        duration,
+      };
+      const stage = await this.stageService.createStage(data);
+      return res.status(200).json(stage);
+    } catch (error) {
+      return res.status(error).json(error);
+    }
+  };
+
+  delete = async (req, res) => {
+    const { idStage } = req.params;
+    try {
+      const stage = await this.stageService.deleteStage(idStage);
+      if (stage) return res.status(200).json(stage);
+      else return res.status(401).json({ error: 'Etapa não encontrada' });
+    } catch (error) {
+      return res.status(500).json({ message: 'Erro ao deletar etapa' });
+    }
+  };
 }
