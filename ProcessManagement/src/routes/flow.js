f3bebfafb8fc7bb68abf58d920698d110fa4dfb5
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-06-05 21:19:21 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#178) - Adiciona endpoint de busca de fluxo por record e corrige erros

--- ProcessManagement/src/routes/flow.js
+++ ProcessManagement/src/routes/flow.js
@@ -3,6 +3,10 @@ import controllers from '../controllers/_index.js';
 
 const FlowRoutes = express.Router();
 FlowRoutes.get('/', controllers.flowController.getAllFlows);
+FlowRoutes.get(
+  '/flows/process/:record',
+  controllers.flowController.indexByRecord,
+);
 FlowRoutes.post('/newFlow', controllers.flowController.store);
 FlowRoutes.delete('/flow/:idFlow', controllers.flowController.delete);
 
