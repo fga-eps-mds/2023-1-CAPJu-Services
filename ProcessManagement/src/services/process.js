9565cb55606a70b2b63af7a3a2191cc8fb3667ea
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-06-05 21:01:45 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#178) - Adiciona serviços de busca de processos por idFlow e Record

--- ProcessManagement/src/services/process.js
+++ ProcessManagement/src/services/process.js
@@ -5,6 +5,14 @@ class ProcessService {
   async getAllProcess() {
     return this.process.findAll();
   }
+
+  async getProcessByRecord(record) {
+    return this.process.findOne({ where: { record } });
+  }
+
+  async getProcessByIdFlow(idFlow) {
+    return this.process.all({ where: { idFlow } });
+  }
 }
 
 export default ProcessService;
