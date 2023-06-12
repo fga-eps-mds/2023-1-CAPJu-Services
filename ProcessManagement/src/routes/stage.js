2a3e15f2be4f8254081c740a1320acbde3da3aaa
Victor Samuel dos Santos Lucas <victor.samuelsantoss@gmail.com>
2023-06-12 11:10:31 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#178) - Corrige erros e implementa padrões nos arquivos de etapas

--- ProcessManagement/src/routes/stage.js
+++ ProcessManagement/src/routes/stage.js
@@ -3,8 +3,11 @@ import controllers from '../controllers/_index.js';
 
 const StageRoutes = express.Router();
 
-StageRoutes.get('/', controllers.stageController.getAllStages);
-StageRoutes.get('/stage/:idStage', controllers.stageController.getStageById);
+StageRoutes.get('/', controllers.stageController.index);
+StageRoutes.get(
+  '/stage/:idStage',
+  controllers.stageController.showStageByStageId,
+);
 StageRoutes.post('/newStage', controllers.stageController.store);
 StageRoutes.delete('/deleteStage/:idStage', controllers.stageController.delete);
 
