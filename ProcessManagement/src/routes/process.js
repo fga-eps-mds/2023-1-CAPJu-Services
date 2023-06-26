5af1146678facd7cfb70882562bdd3486e42a05f
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2023-06-26 14:28:27 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#6) - Adiciona todas as rotas de processo

--- ProcessManagement/src/routes/process.js
+++ ProcessManagement/src/routes/process.js
@@ -2,6 +2,14 @@ import express from 'express';
 import controllers from '../controllers/_index.js';
 const ProcessRoutes = express.Router();
 
-ProcessRoutes.get('/', controllers.processController.getAllProcess);
+ProcessRoutes.get('/', controllers.processController.index);
+ProcessRoutes.get("/:idFlow", controllers.processController.getProcessByIdFlow);
+ProcessRoutes.get("/:record", controllers.processController.getProcessByRecord);
+ProcessRoutes.get("/:idFlow/:record", controllers.processController.getProcessByUniqueKeys);
+ProcessRoutes.post("/newProcess", controllers.processController.store);
+ProcessRoutes.post("/updateProcess", controllers.processController.updateProcess);
+ProcessRoutes.post("/updateStage", controllers.processController.updateProcessStage);
+ProcessRoutes.post("/deleteProcess/:record", controllers.processController.deleteProcess);
+ProcessRoutes.get("/priorities", controllers.processController.getPriorityProcess);
 
 export default ProcessRoutes;
