877921e384e3517c881f504498ef286d88de8ecf
Sergio de Almeida Cipriano Junior <sergiosacj@riseup.net>
2023-06-26 14:32:02 -0300

    (fga-eps-mds/2023-1-CAPJu-Doc#6) - Implementa todas as rotas necessárias para processos

--- ProcessManagement/src/services/process.js
+++ ProcessManagement/src/services/process.js
@@ -2,17 +2,52 @@ class ProcessService {
   constructor(ProcessModel) {
     this.process = ProcessModel;
   }
+
+  async createProcess(params) {
+    return await this.process.create(params);
+  }
+
+  async updateProcess(params) {
+    return await this.process.update(params);
+  }
+
+  async deleteProcessByRecord(record) {
+    return await this.process.destroy({ where: { record } });
+  }
+
   async getAllProcess() {
-    return this.process.findAll();
+    return await this.process.findAll();
+  }
+
+  async getProcessByUniqueKeys(record, idFlow) {
+    return await this.process.findAll({ where: { record, idFlow } });
   }
 
   async getProcessByRecord(record) {
-    return this.process.findAll({ where: { record } });
+    return await this.process.findAll({ where: { record } });
   }
 
   async getProcessByIdFlow(idFlow) {
-    return this.process.findAll({ where: { idFlow } });
+    return await this.process.findAll({ where: { idFlow } });
   }
+
+  validateRecord(record) {
+    const filtered = filterRecord(record);
+    return {
+      filtered,
+      valid: isRecordValid(filtered),
+    };
+  };
+
+  filterRecord(record) {
+    const regex = /[^\d]/g;
+    return record.replace(regex, "");
+  };
+
+  isRecordValid(record) {
+    const regex = /^\d{20}$/;
+    return regex.test(record);
+  };
 }
 
 export default ProcessService;
