9d3219eedc2d9e0704bd626d5f2fc49203e3529a
zpeniel09 <zpeniel09@gmail.com>
2023-07-13 20:15:37 -0300

    Cria o arquivo filters nos utils com formatacao
    Co-authored-by: Vinícius Vieira de Souza <vd.souza1999@gmail.com>

--- ProcessManagement/src/utils/filters.js
+++ ProcessManagement/src/utils/filters.js
@@ -1,7 +1,7 @@
 export function filterByName(req) {
-    return req.query.filter
-      ? {
-          [Op.or]: [{ name: { [Op.like]: `%${req.query.filter}%` } }],
-        }
-      : {};
+  return req.query.filter
+    ? {
+        [Op.or]: [{ name: { [Op.like]: `%${req.query.filter}%` } }],
+      }
+    : {};
 }
