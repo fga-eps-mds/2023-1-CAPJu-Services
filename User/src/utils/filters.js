c027a6503916032211ebc8b6e3d885a728f2bf25
faco400 <vd.souza1999@gmail.com>
2023-07-16 17:34:35 -0300

    atualiza index dos usuarios para ter paginacao

---
+++ User/src/utils/filters.js
@@ -0,0 +1,9 @@
+import { Op } from 'sequelize';
+
+export function filterByFullName(req) {
+  return req.query.filter
+    ? {
+        [Op.or]: [{ fullName: { [Op.like]: `%${req.query.filter}%` } }],
+      }
+    : {};
+}
