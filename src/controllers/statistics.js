8b82a07340a18805bdc43aed6cbd3d8ca69979c9
Denniel <dennielwilliam.unb@gmail.com>
2023-10-19 20:46:34 -0300

    Ajustando lint e prettier

--- src/controllers/statistics.js
+++ src/controllers/statistics.js
@@ -1,48 +1,52 @@
 import services from '../services/_index.js';
 
 export class StatisticsController {
-    constructor(){
-        this.processService = services.processService;
-        this.stageService = services.stageService;
-        this.flowStageService = services.flowStageService;
+  constructor() {
+    this.processService = services.processService;
+    this.stageService = services.stageService;
+    this.flowStageService = services.flowStageService;
+  }
+
+  getProcessByStepInFlow = async (req, res) => {
+    try {
+      // Pega id do fluxo
+      const { idFlow } = req.params;
+
+      if (!idFlow)
+        return res
+          .status(412)
+          .json({ error: 'É necessário fornecer um id de um fluxo' });
+
+      const stages = {};
+
+      // Pega os processos do fluxo
+      const processesbyIdFlow = await this.processService.getProcessByIdFlow(
+        idFlow,
+      );
+
+      // Agrupa os processos por seu idStage e recupera informações da stage
+      for (const item of processesbyIdFlow) {
+        const stage = await this.stageService.findOneByStageId(item.idStage);
+        const key = item?.idStage;
+
+        if (!stages[key ?? 'nao_iniciado'])
+          stages[key ?? 'nao_iniciado'] = { name: '', process: [] };
+
+        stages[key ?? 'nao_iniciado'] = {
+          name: stage?.name ?? 'nao iniciado',
+          idStage: key ?? null,
+          countProcess: stages[key ?? 'nao_iniciado'].process.length,
+          process: stages[key ?? 'nao_iniciado'].process,
+        };
+
+        stages[key ?? 'nao_iniciado'].process.push(item);
+        stages[key ?? 'nao_iniciado'].countProcess =
+          stages[key ?? 'nao_iniciado'].process.length;
+      }
+
+      return res.status(200).json({ stages });
+    } catch (error) {
+      return res.status(500).json({ error: error.message });
     }
-
-    getProcessByStepInFlow = async (req, res) => {
-        try {
-            // Pega id do fluxo
-            const { idFlow } = req.params;
-            
-            if (!idFlow)
-                return res.status(412).json({ error: "É necessário fornecer um id de um fluxo" })
-
-            const stages = {}
-
-            // Pega os processos do fluxo
-            const processesbyIdFlow = await this.processService.getProcessByIdFlow(idFlow);
-
-            // Agrupa os processos por seu idStage e recupera informações da stage
-            for(const item of processesbyIdFlow){
-                const stage = await this.stageService.findOneByStageId(item.idStage);
-                const key = item?.idStage;
-
-                if (!stages[key ?? "nao_iniciado"])
-                    stages[key ?? "nao_iniciado"] = { name: "", process: [] };
-                
-                stages[key ?? "nao_iniciado"] = {
-                    name: stage?.name ?? "nao iniciado",
-                    idStage: key ?? null,
-                    countProcess: stages[key ?? "nao_iniciado"].process.length,
-                    process: stages[key ?? "nao_iniciado"].process
-                };
-
-                stages[key ?? "nao_iniciado"].process.push(item)
-                stages[key ?? "nao_iniciado"].countProcess = stages[key ?? "nao_iniciado"].process.length
-            }
-
-            return res.status(200).json({ stages });
-        } catch (error) {
-            return res.status(500).json({ error: error.message });
-        }
-        
-    }
+  };
 }
--- src/routes/statistics.js
+++ src/routes/statistics.js
@@ -3,8 +3,8 @@ const StatisticsRoutes = express.Router();
 import controllers from '../controllers/_index.js';
 
 StatisticsRoutes.get(
-    '/qtdFlow/:idFlow',
-    controllers.statisticsController.getProcessByStepInFlow,
+  '/qtdFlow/:idFlow',
+  controllers.statisticsController.getProcessByStepInFlow,
 );
 
 export default StatisticsRoutes;
