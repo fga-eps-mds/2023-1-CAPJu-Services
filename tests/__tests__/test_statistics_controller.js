f84984bf4e03f814b0ec7b9fd645e560011b068c
Washington Bispo <juniorbispo515@gmail.com>
2023-11-30 14:52:42 -0300

    Finalizando testes da controller da statistics

--- tests/__tests__/test_statistics_controller.js
+++ tests/__tests__/test_statistics_controller.js
@@ -50,6 +50,60 @@ describe('statisticsController', () => {
     expect(resMock.status).toHaveBeenCalledWith(200);
   });
 
+  test('getProcessByStepInFlow - process found - second ternary (200)', async () => {
+    const mockProcesses = {
+      count: 0,
+      rows: [
+        { id: 1, name: 'Process 1', idStage: 1, idFlow: 1 },
+        { id: 2, name: 'Process 2', idStage: 1, idFlow: 1 },
+      ],
+    };
+
+    const getAndCountAllProcessSpy = jest
+      .spyOn(ProcessService.prototype, 'getAndCountAllProcess')
+      .mockResolvedValue(mockProcesses);
+
+    reqMock.params = { idFlow: 1, idStage: 1 };
+    reqMock.query = {};
+
+    await statisticsController.getProcessByStepInFlow(reqMock, resMock);
+
+    expect(getAndCountAllProcessSpy).toHaveBeenCalled();
+    expect(resMock.json).toHaveBeenCalledWith({
+      process: mockProcesses.rows,
+      totalPages: 0,
+    });
+    expect(resMock.status).toHaveBeenCalledWith(200);
+  });
+
+  test('getProcessByStepInFlow - no idFlow (412)', async () => {
+    reqMock.params = { idStage: 1 };
+    reqMock.query = { offset: 0, limit: 5 };
+
+    await statisticsController.getProcessByStepInFlow(reqMock, resMock);
+
+    expect(resMock.json).toHaveBeenCalledWith(
+      {
+        error: 'É necessário fornecer um id de um fluxo' 
+      }
+    );
+    expect(resMock.status).toHaveBeenCalledWith(412);
+  });
+
+  test('getProcessByStepInFlow - no idStage (412)', async () => {
+    reqMock.params = { idFlow: 1 };
+    reqMock.query = { offset: 0, limit: 5 };
+
+    await statisticsController.getProcessByStepInFlow(reqMock, resMock);
+
+    expect(resMock.json).toHaveBeenCalledWith(
+      {
+        error: 'É necessário fornecer um id de uma etapa' 
+      }
+    );
+    expect(resMock.status).toHaveBeenCalledWith(412);
+  });
+
   test('getProcessByStepInFlow - error (500)', async () => {
     const error = new Error();
 
@@ -98,6 +152,118 @@ describe('statisticsController', () => {
     expect(resMock.status).toHaveBeenCalledWith(200);
   });
 
+  test('getProcessCountByStepInFlow - process found (200)', async () => {
+    const mockProcesses = [
+      { id: 1, name: 'Process 1', idFlow: 1 },
+    ];
+
+
+    const mockStages = { };
+
+    const getProcessByIdFlowSpy = jest
+      .spyOn(ProcessService.prototype, 'getProcessByIdFlow')
+      .mockResolvedValue(mockProcesses);
+    const findOneByStageIdSpy = jest
+      .spyOn(StageService.prototype, 'findOneByStageId')
+      .mockResolvedValue(mockStages);
+
+    reqMock.params = { idFlow: 1 };
+
+    await statisticsController.getProcessCountByStepInFlow(reqMock, resMock);
+
+    expect(getProcessByIdFlowSpy).toHaveBeenCalled();
+    expect(findOneByStageIdSpy).toHaveBeenCalled();
+    expect(resMock.json).toHaveBeenCalledWith({
+      stages: {
+        nao_iniciado: {
+          idStage: "nao_iniciado",
+          name: "nao iniciado",
+          countProcess: 1,
+        },
+      },
+    });
+    expect(resMock.status).toHaveBeenCalledWith(200);
+  });
+
+  test('getProcessCountByStepInFlow - process found (200)', async () => {
+    const mockProcesses = [
+      { id: 1, name: 'Process 1', idStage: null, idFlow: 1 },
+    ];
+
+
+    const mockStages = { };
+
+    const getProcessByIdFlowSpy = jest
+      .spyOn(ProcessService.prototype, 'getProcessByIdFlow')
+      .mockResolvedValue(mockProcesses);
+    const findOneByStageIdSpy = jest
+      .spyOn(StageService.prototype, 'findOneByStageId')
+      .mockResolvedValue(mockStages);
+
+    reqMock.params = { idFlow: 1 };
+
+    await statisticsController.getProcessCountByStepInFlow(reqMock, resMock);
+
+    expect(getProcessByIdFlowSpy).toHaveBeenCalled();
+    expect(findOneByStageIdSpy).toHaveBeenCalled();
+    expect(resMock.json).toHaveBeenCalledWith({
+      stages: {
+        nao_iniciado: {
+          idStage: "nao_iniciado",
+          name: "nao iniciado",
+          countProcess: 1,
+        },
+      },
+    });
+    expect(resMock.status).toHaveBeenCalledWith(200);
+  });
+
+  test('getProcessCountByStepInFlow - process found (200)', async () => {
+    const mockProcesses = [
+      { id: 1, name: 'Process 1', idStage: 1, idFlow: 1 },
+      { id: 2, name: 'Process 2', idStage: 1, idFlow: 1 },
+    ];
+
+    const mockStages = { name: 'Etapa01' };
+
+    const getProcessByIdFlowSpy = jest
+      .spyOn(ProcessService.prototype, 'getProcessByIdFlow')
+      .mockResolvedValue(mockProcesses);
+    const findOneByStageIdSpy = jest
+      .spyOn(StageService.prototype, 'findOneByStageId')
+      .mockResolvedValue(mockStages);
+
+    reqMock.params = { idFlow: 1 };
+
+    await statisticsController.getProcessCountByStepInFlow(reqMock, resMock);
+
+    expect(getProcessByIdFlowSpy).toHaveBeenCalled();
+    expect(findOneByStageIdSpy).toHaveBeenCalled();
+    expect(resMock.json).toHaveBeenCalledWith({
+      stages: {
+        1: {
+          name: 'Etapa01',
+          idStage: 1,
+          countProcess: 2,
+        },
+      },
+    });
+    expect(resMock.status).toHaveBeenCalledWith(200);
+  });
+
+  test('getProcessCountByStepInFlow - process no idFlow (412)', async () => {
+    reqMock.params = {};
+
+    await statisticsController.getProcessCountByStepInFlow(reqMock, resMock);
+
+    expect(resMock.json).toHaveBeenCalledWith(
+    { 
+        error: 'É necessário fornecer um id de um fluxo' 
+    }
+    );
+    expect(resMock.status).toHaveBeenCalledWith(412);
+  });
+
   test('getProcessCountByStepInFlow - error (500)', async () => {
     const mockProcesses = [
       { id: 1, name: 'Process 1', idStage: 1, idFlow: 1 },
@@ -142,6 +308,25 @@ describe('statisticsController', () => {
     expect(resMock.status).toHaveBeenCalledWith(200);
   });
 
+  test('getAllProcessByStepInStage - process no idFlow (412)', async () => {
+   reqMock.params = { idStage: 1 };
+
+    await statisticsController.getAllProcessByStepInStage(reqMock, resMock);
+
+    expect(resMock.json).toHaveBeenCalledWith({ error: 'É necessário fornecer um id de um fluxo' });
+    expect(resMock.status).toHaveBeenCalledWith(412);
+  });
+
+  test('getAllProcessByStepInStage - process no idStage (412)', async () => {
+    reqMock.params = { idFlow: 1};
+
+    await statisticsController.getAllProcessByStepInStage(reqMock, resMock);
+
+
+    expect(resMock.json).toHaveBeenCalledWith({ error: 'É necessário fornecer um id de uma etapa' });
+    expect(resMock.status).toHaveBeenCalledWith(412);
+  });
+
   test('getAllProcessByStepInStage - error (500)', async () => {
     const error = new Error();
 
@@ -155,5 +340,163 @@ describe('statisticsController', () => {
 
     expect(getAllProcessSpy).toHaveBeenCalled();
     expect(resMock.status).toHaveBeenCalledWith(500);
+  });
+
+  test('getProcessByDueDateInFlow - process found (200)', async () => {
+    const mockProcesses = [
+      { record: '4905241-90.2023.6.17.7722',
+        nickname: 'Process 1',
+        idStage: 1,
+        idFlow: 1,
+        status: 'inProgess',
+        idUnit: 1,
+        effectiveDate: '11/24/2023',
+        nameStage: 'Etapa 1',
+        nameFlow: 'Fluxo 1',
+        dueDate: '11/25/2023 02:34:08.878+00'
+    }
+    ];
+
+    const mockResponse = [
+      { record: '4905241-90.2023.6.17.7722',
+        nickname: 'Process 1',
+        idStage: 1,
+        idFlow: 1,
+        status: 'inProgess',
+        idUnit: 1,
+        effectiveDate: '11/24/2023',
+        nameStage: 'Etapa 1',
+        nameFlow: 'Fluxo 1',
+        dueDate: '24/11/2023'
+    }
+    ];
+    reqMock.query = { offset:5, limit: 10}
+    reqMock.params = { minDate: '11/23/2023', maxDate: '11/26/2023' };
+
+    statisticsController.statisticsService.SearchDueDate = jest
+    .fn()
+    .mockResolvedValue(mockProcesses);
+
+    statisticsController.statisticsService.countRowsDueDate = jest
+    .fn()
+    .mockResolvedValue(mockProcesses.length);
+
+    await statisticsController.getProcessByDueDateInFlow(reqMock,resMock);
+
+    expect(resMock.status).toHaveBeenCalledWith(200);
+    expect(resMock.json).toHaveBeenCalledWith(
+      {
+        processInDue: mockResponse,
+        totalPages: 1
+      }
+    );
+  });
+
+  test('getProcessByDueDateInFlow - process found (200)', async () => {
+    const mockProcesses = [
+      { record: '4905241-90.2023.6.17.7722',
+        nickname: 'Process 1',
+        idStage: 1,
+        idFlow: 1,
+        status: 'inProgess',
+        idUnit: 1,
+        effectiveDate: '11/24/2023',
+        nameStage: 'Etapa 1',
+        nameFlow: 'Fluxo 1',
+        dueDate: '11/25/2023 02:34:08.878+00'
+    }
+    ];
+
+    const mockResponse = [
+      { record: '4905241-90.2023.6.17.7722',
+        nickname: 'Process 1',
+        idStage: 1,
+        idFlow: 1,
+        status: 'inProgess',
+        idUnit: 1,
+        effectiveDate: '11/24/2023',
+        nameStage: 'Etapa 1',
+        nameFlow: 'Fluxo 1',
+        dueDate: '24/11/2023'
+    }
+    ];
+    reqMock.query = { offset:5, limit: 10}
+    reqMock.params = { minDate: '11/23/2023', maxDate: '11/26/2023' };
+
+    statisticsController.statisticsService.SearchDueDate = jest
+    .fn()
+    .mockResolvedValue(mockProcesses);
+
+    statisticsController.statisticsService.countRowsDueDate = jest
+    .fn()
+    .mockResolvedValue(mockProcesses.length);
+
+    await statisticsController.getProcessByDueDateInFlow(reqMock,resMock);
+
+    expect(resMock.status).toHaveBeenCalledWith(200);
+    expect(resMock.json).toHaveBeenCalledWith(
+      {
+        processInDue: mockResponse,
+        totalPages: 1
+      }
+    );
+  });
+
+  test('getProcessByDueDateInFlow - process found - second ternary (200)', async () => {
+    const mockProcesses = [];
+
+    const mockResponse = [];
+    reqMock.query = {}
+    reqMock.params = { minDate: '11/23/2023', maxDate: '11/26/2023' };
+
+    statisticsController.statisticsService.SearchDueDate = jest
+    .fn()
+    .mockResolvedValue(mockProcesses);
+
+    statisticsController.statisticsService.countRowsDueDate = jest
+    .fn()
+    .mockResolvedValue(mockProcesses.length);
+
+    await statisticsController.getProcessByDueDateInFlow(reqMock,resMock);
+
+    expect(resMock.status).toHaveBeenCalledWith(200);
+    expect(resMock.json).toHaveBeenCalledWith(
+      {
+        processInDue: mockResponse,
+        totalPages: 1
+      }
+    );
+  });
+
+  test('getProcessByDueDateInFlow - error (500)', async () => {
+    const error = new Error('Internal Server Error');
+
+    reqMock.query = { offset:5, limit: 10}
+    reqMock.params = { minDate: '11/23/2023', maxDate: '11/26/2023' };
+
+    statisticsController.statisticsService.SearchDueDate = jest
+    .fn()
+    .mockRejectedValue(error);
+
+    await statisticsController.getProcessByDueDateInFlow(reqMock,resMock);
+
+    expect(resMock.status).toHaveBeenCalledWith(500);
+    expect(resMock.json).toHaveBeenCalledWith(
+      { message: error }
+    );
+  });
+
+  test('getProcessByDueDateInFlow - no minDate and maxDate (412)', async () => {
+    reqMock.query = { offset:5, limit: 10}
+    reqMock.params = {}
+
+    await statisticsController.getProcessByDueDateInFlow(reqMock,resMock);
+
+    expect(resMock.status).toHaveBeenCalledWith(412);
+    expect(resMock.json).toHaveBeenCalledWith(
+      { 
+        error: 'É necessário indicar o período de vencimento!' 
+      }
+    );
   });
 });
