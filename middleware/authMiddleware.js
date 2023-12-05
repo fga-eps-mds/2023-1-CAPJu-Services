6cf0bf02ff88a55c9f67f7392b98a0d65ac3a96e
ngm1450 <georgeos.nicolas83@gmail.com>
2023-12-04 22:02:52 -0300

    correções testes

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -3,6 +3,8 @@ import jwt from 'jsonwebtoken';
 import UserEndpointAccessLogModel from '../src/models/userEndpointAccessLog.js';
 import services from '../src/services/_index.js';
 import routesPermissions from '../src/routes/routesPermissions.js';
+import sequelizeConfig from '../src/config/sequelize.js';
+import { QueryTypes } from 'sequelize';
 
 const publicEndpoints = [
   /^\/login$/,
@@ -12,11 +14,13 @@ const publicEndpoints = [
 ];
 
 async function authenticate(req, res, next) {
-  const isPublicEndpoint = publicEndpoints.some(pattern => pattern.test(req.originalUrl));
+  const isPublicEndpoint = publicEndpoints.some(pattern =>
+    pattern.test(req.originalUrl),
+  );
   let isAccepted = true;
   let message = null;
 
-  if(isPublicEndpoint) {
+  if (isPublicEndpoint) {
     await registerEndpointLogEvent({ req, isAccepted, message });
     next();
     return;
@@ -32,22 +36,33 @@ async function authenticate(req, res, next) {
       const token = authorizationHeader.split(' ')[1];
       const decodedUser = jwt.verify(token, process.env.JWT_SECRET).id;
 
-      const userData = await services.userService.getUserByCpfWithPasswordRolesAndUnit(decodedUser.cpf);
+      const userData =
+        await services.userService.getUserByCpfWithPasswordRolesAndUnit(
+          decodedUser.cpf,
+        );
 
       if (!userData || userData.accepted === false) {
         throw new Error('');
       }
 
-      const hasActiveSession = await services.userAccessLogService.hasActiveSessionRelatedToJWT(token);
+      const hasActiveSession =
+        await services.userAccessLogService.hasActiveSessionRelatedToJWT(token);
       if (!hasActiveSession) {
         throw new Error('Token não associado a uma sessão ativa.');
       }
 
-      ({ isAccepted, message } = checkPermissions({ req, isAccepted, message, userData }));
-
+      ({ isAccepted, message } = checkPermissions({
+        req,
+        isAccepted,
+        message,
+        userData,
+      }));
     } catch (error) {
       isAccepted = false;
-      message = error.name === 'TokenExpiredError' ? 'O token expirou!' : (error.message || 'Autenticação falhou!');
+      message =
+        error.name === 'TokenExpiredError'
+          ? 'O token expirou!'
+          : error.message || 'Autenticação falhou!';
     }
   }
 
@@ -65,15 +80,14 @@ function getRequiredPermissions(req) {
   let matchingPermissions = null;
   let wasFound = false;
   for (let parentRoute of routesPermissions) {
-    if(wasFound)
-      break;
+    if (wasFound) break;
     for (const childRoute of parentRoute.childRoutes) {
-      const fullPath = parentRoute.parentPath + (childRoute.path === '' ? '' : childRoute.path);
+      const fullPath =
+        parentRoute.parentPath +
+        (childRoute.path === '' ? '' : childRoute.path);
       const regexPath = fullPath.replace(/\/:[^\/]+/g, '/[^/]+');
       const regex = new RegExp(`^${regexPath}$`);
       if (regex.test(requestPath) && childRoute?.method === req.method) {
-        console.log(requestPath)
-        console.log(childRoute);
         matchingPermissions = childRoute.permissions;
         wasFound = true;
         break;
@@ -85,9 +99,13 @@ function getRequiredPermissions(req) {
 
 function checkPermissions({ req, isAccepted, message, userData }) {
   let requiredPermissions = getRequiredPermissions(req);
-  if(requiredPermissions) {
-    requiredPermissions = Array.isArray(requiredPermissions) ? requiredPermissions : [requiredPermissions];
-    if (!requiredPermissions.every(p => userData.role.allowedActions.includes(p))) {
+  if (requiredPermissions) {
+    requiredPermissions = Array.isArray(requiredPermissions)
+      ? requiredPermissions
+      : [requiredPermissions];
+    if (
+      !requiredPermissions.every(p => userData.role.allowedActions.includes(p))
+    ) {
       isAccepted = false;
       message = 'Permissão negada!';
     }
@@ -104,24 +122,58 @@ async function registerEndpointLogEvent({ req, isAccepted, message }) {
   let userCPF;
   try {
     userCPF = (await userFromReq(req)).cpf;
-  } catch (e) { userCPF = null; }
+  } catch (e) {
+    userCPF = null;
+  }
   try {
     await UserEndpointAccessLogModel.create(
-    {
-      endpoint: req.originalUrl,
-      httpVerb: req.method,
-      attemptTimestamp: new Date(),
-      userCPF,
-      isAccepted,
-      message,
-      service: 'User',
-    },
-    {
-      logging: false,
-    });
+      {
+        endpoint: req.originalUrl,
+        httpVerb: req.method,
+        attemptTimestamp: new Date(),
+        userCPF,
+        isAccepted,
+        message,
+        service: 'User',
+      },
+      {
+        logging: false,
+      },
+    );
   } catch (error) {
     console.error('Error logging request: ', error);
   }
 }
 
-export { authenticate, userFromReq };
+async function tokenToUser(req, res) {
+  let token;
+  if (
+    req.headers.authorization &&
+    req.headers.authorization.startsWith('Bearer')
+  ) {
+    try {
+      token = req.headers.authorization.split(' ')[1];
+
+      const decoded = jwt.verify(token, process.env.JWT_SECRET);
+      const userData = await sequelizeConfig.query(
+        `select * \ 
+      from \
+          users u \
+      where \
+          cpf = '${decoded.id}'`,
+        {
+          type: QueryTypes.SELECT,
+        },
+      );
+
+      if (userData[0].accepted === false) {
+        throw new Error();
+      }
+      return userData[0];
+    } catch (error) {
+      return res.status(401);
+    }
+  }
+}
+
+export { authenticate, userFromReq, tokenToUser };
