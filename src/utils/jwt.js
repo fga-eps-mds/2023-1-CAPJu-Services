3af63a9382a95a02e18aa0ad8d4486320c0b3287
ngm1450 <georgeos.nicolas83@gmail.com>
2023-12-07 17:38:46 -0300

    limpeza
    Co-authored-by: Daniel Rodrigues <danielrocha.dr142@gmail.com>
    Co-authored-by: Camila Careli <camilacsouzza@gmail.com>

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -5,6 +5,7 @@ export const jwtToken = process.env.JWT_SECRET || 'ABC';
 
 export const generateToken = id => {
   return jwt.sign({ id }, jwtToken, {
-    expiresIn: parseInt(process.env.JWT_EXPIRATION_TIME_IN_MINUTES) + 'min',
+    expiresIn:
+      parseInt(process.env.JWT_EXPIRATION_TIME_IN_MINUTES) || 600 + 'min',
   });
 };
