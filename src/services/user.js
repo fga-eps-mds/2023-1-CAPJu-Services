e8bcdd539ef075e1279cbb1cb1316eb277d4f260
M0tt1nh4 <felipemotta949@gmail.com>
2023-12-09 17:24:21 -0300

    Arrumando testes quebrados.

--- src/services/user.js
+++ src/services/user.js
@@ -153,7 +153,6 @@ class UserService {
       );
       if (isPasswordCorrect) {
         const hashedPassword = await hash(newPassword, passHashing);
-        console.log('----> SENHA ', hashedPassword);
         const [updatedRows] = await this.user.update(
           { password: hashedPassword },
           { where: { cpf: cpfFilter(cpf) } },
