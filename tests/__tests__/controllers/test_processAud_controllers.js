3e482e1ef58fe2c20f49970198074be7e9712040
Bruno Ricardo <brunomenezes1511@gmail.com>
2023-12-10 16:36:31 -0300

    Testando função findAll
    Co-authored-by: Bispo <WashingtonBispo@users.noreply.github.com>

--- tests/__tests__/controllers/test_processAud_controllers.js
+++ tests/__tests__/controllers/test_processAud_controllers.js
@@ -54,4 +54,41 @@ describe('findAllPaged', () => {
       expect(resMock.status).toHaveBeenCalledWith(500);
   });
 })
+
+describe('findAll', () => {  
+    test('Status - 200', async () => {
+        const mockProcessAud = [{
+          id: 1,
+          idProcess: 1,
+          processRecord: 1234567890,
+          operation: 'INSERT',
+          changedAt: '12/12/2022',
+          changedBy: '12345678901',
+          oldValues: '',
+          newValues: '{\"idStage\":null,\"status\":\"notStarted\",\"idProcess\":3659,\"record\":\"16996097420235058389\",\"idUnit\":1,\"nickname\":\"processo 02\",\"idFlow\":10,\"idPriority\":0,\"finalised\":false,\"updatedAt\":\"2023-12-10T15:43:59.828Z\",\"createdAt\":\"2023-12-10T15:43:59.828Z\",\"effectiveDate\":null}'
+        }]
+
+        processAudController.processAudService.findAll = jest
+        .fn()
+        .mockResolvedValue(mockProcessAud);
+        
+        await processAudController.findAll(reqMock, resMock);
+        
+        expect(resMock.json).toHaveBeenCalledWith(mockProcessAud);
+        expect(resMock.status).toHaveBeenCalledWith(200);
+    });
+
+    test('Status - 500', async () => {
+      const mockProcessAud = new Error('Internal Server Error')
+
+      processAudController.processAudService.findAll = jest
+      .fn()
+      .mockRejectedValue(mockProcessAud);
+      
+      await processAudController.findAll(reqMock, resMock);
+      
+      expect(resMock.json).toHaveBeenCalledWith({ error: `${mockProcessAud}`, message: `Erro ao buscar histórico` });
+      expect(resMock.status).toHaveBeenCalledWith(500);
+    });
+})
 });
