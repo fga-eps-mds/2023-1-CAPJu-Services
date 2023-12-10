fd2f6cd64b9986157db0770310ac7d5ca9a6bbda
Bruno Ricardo <brunomenezes1511@gmail.com>
2023-12-10 20:11:08 -0300

    Testando função updateFileItem

--- tests/__tests__/controllers/test_processesFile.js
+++ tests/__tests__/controllers/test_processesFile.js
@@ -124,6 +124,58 @@ describe('create', () => {
     });
 }); 
 
+describe('updateFileItem', () => {
+    test('Status - 200', async () => {
+        const mockProcessFile = {
+            idProcessFile: 1,
+            name: 'teste',
+            status: 'failed',
+            importedBy: 12345678901,
+            importedAt: '12/12/2022',
+            fileName: 'filename.csv',
+        };
+        const params = { idProcessFile: 1 };
+        const body = {
+          idProcessFile: 1,
+          name: 'teste',
+          status: 'failed',
+          importedBy: 12345678901,
+          importedAt: '12/12/2022',
+          fileName: 'filename.csv',
+        };
+
+        reqMock.body = body;
+        reqMock.params = params;
+        resMock.json = jest.fn().mockResolvedValue(mockProcessFile);
+        resMock.status = jest.fn(() => resMock);
+
+        processesFileController.processesFileService.updateFileItem = jest
+        .fn()
+        .mockResolvedValue([mockProcessFile.idProcessFile]);
+        
+        const result = await processesFileController.updateFileItem(reqMock, resMock);
+        
+        expect(result).toEqual(mockProcessFile)
+    });
+    
+
+    test('Status - 500', async () => {
+        const mockProcessFileError = new Error('Internal Server Error');
+    
+        processesFileController.processesFileService.updateFileItem = jest
+            .fn()
+            .mockRejectedValue(mockProcessFileError);
+    
+        await processesFileController.updateFileItem(reqMock, resMock);
+    
+        expect(resMock.json).toHaveBeenCalledWith({
+            error: 'Error: Internal Server Error',
+            message: 'Erro ao atualizar item',
+        });
+        expect(resMock.status).toHaveBeenCalledWith(500);
+    });
+}); 
+
 describe('findFileById', () => {
     test('Status - 200', async () => {
         const mockProcessFile = {
