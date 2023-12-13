85f876a602ceaff5edf90d70495ef48882924347
ngm1450 <georgeos.nicolas83@gmail.com>
2023-12-13 12:27:19 -0300

    correção expiração jwt

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -5,6 +5,7 @@ export const jwtToken = process.env.JWT_SECRET || 'capju_secret';
 
 export const generateToken = id => {
   const iat = Math.floor(Date.now() / 1000);
-  const exp = iat + parseInt(process.env.JWT_EXPIRATION_TIME_IN_MINUTES) * 60;
+  const exp =
+    iat + parseInt(process.env.JWT_EXPIRATION_TIME_IN_MINUTES || 600) * 60;
   return jwt.sign({ id, exp }, jwtToken);
 };
